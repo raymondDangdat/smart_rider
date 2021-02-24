4cb14f9c79ca6bab71251d1175854a6ac82ad73d
Dangdat <raymonddangdat@gmail.com>
2021-02-24 20:09:17 +0100

    Implemented Login UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_rider/allScreens/login_screen.dart';
 import 'package:smart_rider/allScreens/main_screen.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MainScreen(),
+      home: LoginScreen(),
     );
   }
 }
